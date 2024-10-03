1b92ca22da9b016f4fd68e2cb613a8bc7f43650d
Vishal Narkhede <104969449+vishal230503@users.noreply.github.com>
2024-10-03 20:23:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,10 +3,11 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-ami = "ami-03eb6185d756497f8"
-instance_type = "t2.micro"
-tags = {
-Name = "dev-server"
-}
-
+  ami           = "ami-00f251754ac5da7f0"
+  instance_type = "t2.micro"
+  key_name      = "webserver"
+  vpc_security_group_ids = ["sg-05ef1a9343c8a2163"]
+  tags = {
+    Name = "test-server"
+  }
 }
