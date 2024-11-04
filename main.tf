531d07b75c94315c659b40d748f9c4d174d9c94e
Vishal Narkhede <104969449+vishal230503@users.noreply.github.com>
2024-11-04 18:10:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-count = 5
+count = 2
 ami = "ami-0ddc798b3f1a5117e"
-instance_type = "t2.micro"
+instance_type = "t2.medium"
 key_name = "webserver"
 vpc_security_group_ids = ["sg-05ef1a9343c8a2163"]
 tags = {
@@ -14,5 +14,5 @@ Name = var.instance_names[count.index]
 }
 
 variable "instance_names" {
-default = ["ansible", "dev-1", "dev-2", "test-1", "test-2"]
+default = ["nexus", "jenkins"]
 }
