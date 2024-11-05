55249878436bd8343165cfa35115523596adea27
Vishal Narkhede <104969449+vishal230503@users.noreply.github.com>
2024-11-05 12:44:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-count = 2
+count = 1
 ami = "ami-0ddc798b3f1a5117e"
 instance_type = "t2.medium"
 key_name = "webserver"
@@ -14,5 +14,5 @@ Name = var.instance_names[count.index]
 }
 
 variable "instance_names" {
-default = ["nexus", "jenkins"]
+default = ["nexus"]
 }
