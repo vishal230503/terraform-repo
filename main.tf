437e2de454510cbc370ae6655aa79b44597654c9
Vishal Narkhede <104969449+vishal230503@users.noreply.github.com>
2025-01-07 10:31:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 provider "aws" {
-region = "us-east-1"
+region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
-count = 4
-ami = "ami-0ddc798b3f1a5117e"
-instance_type = "t2.medium"
-key_name = "webserver"
-vpc_security_group_ids = ["sg-05ef1a9343c8a2163"]
+count = 5
+ami = "ami-021e165d8c4ff761d"
+instance_type = "t2.micro"
+key_name = "mircoservices"
+vpc_security_group_ids = ["sg-039089578050dc81c"]
 tags = {
 Name = var.instance_names[count.index]
 }
 }
 
 variable "instance_names" {
-default = ["tomcat-2", "tomcat-1", "nexus", "jenkins"]
+default = ["dev-1", "dev-2", "ansible", "test-1", "test-2"]
 }
